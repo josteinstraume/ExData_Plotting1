dac03523e1167d77da6dd7ae5bc595e82435443a
Jostein Straume <joebarrystraume@gmail.com>
2018-02-06 15:50:13 -0800

    Created 4 requested plots

---
+++ analysis/analysis.R
@@ -0,0 +1,59 @@
+library(lubridate)
+library(ggplot2)
+filePath <- '/Users/Jostein/stories/ExData_Plotting1/data/household_power_consumption.txt'
+df <- read.csv2(filePath, header = TRUE)
+
+# Check out the structure of the dataframe
+str(df)
+
+# Create new attribute to convert date attribute into a Date class
+df$Date2 <- dmy(df$Date)
+
+# Transform into date time class
+df$Time2 <- strptime(paste(df$Date, df$Time), "%d/%m/%Y %H:%M:%S")
+#df$Date2 <- strptime(df$Date, format="%y-%m-%d, %I:%M%p")
+
+# Create new dataset as a subset of the original within the 2 day period
+newDf <- df["2007-02-01" <= df$Date2 & df$Date2 <= "2007-02-02",]
+
+# Confirm new dataset is within the 2 day period
+range(newDf$Date2)
+
+#ggplot(newDf, aes(newDf$Global_active_power)) + geom_histogram(stat="count")
+
+# Convert global active power to class numeric for histogram
+newDf$Global_active_power <- as.numeric(newDf$Global_active_power)
+
+# Plot 1
+png("plot1.png", width=480, height=480)
+hist(newDf$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "Red")
+dev.off
+
+# Plot 2
+png("plot2.png", width=480, height=480)
+plot(newDf$Time2, newDf$Global_active_power, ylab = "Global Active Power (kilowatts)", type = "l", xlab = " ")
+dev.off
+
+# Plot 3
+png("plot3.png", width=480, height=480)
+plot(newDf$Time2, as.numeric(newDf$Sub_metering_1), xlab = "", ylab = "Energy Sub Metering", type = "l", col = "Black")
+legend("topright", col = c("Black", "Red", "Blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1)
+lines(newDf$Time2, as.numeric(newDf$Sub_metering_2), col = "Red")
+lines(newDf$Time2, as.numeric(newDf$Sub_metering_3), col = "Blue")
+dev.off
+
+# Plot 4
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2,2))
+hist(newDf$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "Red")
+
+plot(newDf$Time2, newDf$Global_active_power, ylab = "Global Active Power (kilowatts)", type = "l", xlab = " ")
+
+plot(newDf$Time2, as.numeric(newDf$Sub_metering_1), xlab = "", ylab = "Energy Sub Metering", type = "l", col = "Black")
+legend("topright", box.lwd = 0, col = c("Black", "Red", "Blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1)
+lines(newDf$Time2, as.numeric(newDf$Sub_metering_2), col = "Red")
+lines(newDf$Time2, as.numeric(newDf$Sub_metering_3), col = "Blue")
+
+plot(newDf$Time2, newDf$Global_reactive_power, xlab = "date time", ylab = "Global Reactive Power", type = "l")
+lines(newDf$Time2, newDf$Global_reactive_power)
+dev.off
